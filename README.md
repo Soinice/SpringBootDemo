235db4996892baa33ad8cdc752909ac9b85c4abc
meng <lvym@huazhenit.com>
2018-09-11 09:54:55 +0800

    Support JSP

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # springBootDemo
-simple spring boot
+simple spring boot web chapter：
+> - 2018-9-10 15:11:30 ：Support JSP
--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author lvyimeng
@@ -10,12 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: 你好，世界。
  * @date: 2018/9/3 16:47
  */
-@RestController
+@Controller
 @RequestMapping("/index")
 public class IndexController {
+
+    /**
+     * 返回数据
+     *
+     * @return
+     */
+    @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
         System.out.println("返回数据" + ">>>>>>>>>>>>>>>>>>>>>>>>>>>");
         return "你好，世界。";
+    }
+
+    /**
+     *  跳转JSP界面
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping("/helloJSP")
+    public String helloJSP(HttpServletRequest request) {
+        System.out.println("跳转JSP界面" + ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        request.setAttribute("search","Spring Boot （Web 篇）：支持JSP");
+        return "indexJSP";
     }
 }
